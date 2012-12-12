429c9186239c0d3bf1d97b9af62917184b4b20a6
unknown <Himanshu Gupta@XFT000075505001.ads.mckinsey.com>
2012-12-12 18:35:37 +0530

    Rails basic overview assignment

---
+++ Rails_Overview_Assignment/railsApp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsApp::Application.config.secret_token = '51146f59e564cdafc4460999ceb4230c03b577861d533ed873a434f521b40d5d7d96f1ca7addc85c249317e9868d4c84570989f5bca33d3c3bfa40e18b7949c8'
---
+++ Rails_Overview_Assignment/sampleApp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'ca0fc62f76df9bb6b4664bfa32efe04a61791530fa93db1877ef80b52e21b330daef9b6cf37f394e2083a8affa9487451b586a17aafced7b750962579800a039'
