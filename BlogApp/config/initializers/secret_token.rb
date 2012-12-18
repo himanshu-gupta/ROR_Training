0a09fa52eddba47a5a802cf4518ab4d1428ab825
unknown <Himanshu Gupta@XFT000075505001.ads.mckinsey.com>
2012-12-18 14:21:58 +0530

    Heroku push

---
+++ BlogApp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogApp::Application.config.secret_token = '52e4fa71d3527b7e8727f0d3e406764c866d7b972ec4c6fbcd038e549031141c2bd2d748630a25634107fbd8599edefb9e88301f71a037ef6f87c83c14ce9c21'
